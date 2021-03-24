c9d398286514153e173a2cb784cf06cdfcf18e1c
sandra1995linnea <sandralinneae@gmail.com>
2021-03-24 22:55:20 +0100

    finsihed possibletiles method

--- Lab 2/Sudoku/Sudoku/Board.cs
+++ Lab 2/Sudoku/Sudoku/Board.cs
@@ -7,7 +7,7 @@ namespace Sudoku
     {
         readonly Tile[,] board;
 
-        private List<Tuple<int, int>> emptyTiles;
+        private List<Tuple<int, int>> emptySpots = new List<Tuple<int, int>>();
 
         public Board(List<Tile> tiles)
         {
@@ -18,18 +18,111 @@ namespace Sudoku
                 for (int j = 0; j < 9; j++)
                 {
                     board[i, j] = tiles[0];
+                    if(tiles[0].Number == 0) // empty
+                    {
+                        emptySpots.Add(new Tuple<int, int>(i, j));
+                    }
                     tiles.RemoveAt(0);
                 }
             }
-
-            // TODO fill emptyTiles
         }
 
-        /*private List<Tile> PossibleTiles(Tuple<int, int> spot)
+        /// <summary>
+        /// Will return a list of all tiles that could be placed on a spot
+        /// </summary>
+        /// <param name="spot">A spot, i.e. a Tuple of row and column</param>
+        /// <returns>List of tiles</returns>
+        private List<Tile> PossibleTiles(Tuple<int, int> spot)
         {
+            List<Tile> tiles = new List<Tile>();
+            
+            for(int number = 1; number <= 9; number++)
+            {
+                bool found = false;
+                /* 1st: check the row from 0 to 9
+                 * 2nd check the column from 0 to 9
+                 * 3rd check the grid */
 
+                // searching through the row:
+                for(int j = 0; j < 9; j++)
+                {
+                    if(board[spot.Item1, j].Number == number)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if(found)
+                {
+                    continue;
+                }
+
+                // searching through the column
+                for (int i = 0; i < 9; i++)
+                {
+                    if(board[i, spot.Item2].Number == number)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if(found)
+                {
+                    continue;
+                }
+
+                // searching through the mini grid
+                int startrow;
+                int startcolumn;
+
+                if(spot.Item1 < 3)
+                {
+                    startrow = 0;
+                }
+                else if(spot.Item1 < 6)
+                {
+                    startrow = 3;
+                }
+                else
+                {
+                    startrow = 6;
+                }
+
+                if (spot.Item2 < 3)
+                {
+                    startcolumn = 0;
+                }
+                else if (spot.Item2 < 6)
+                {
+                    startcolumn = 3;
+                }
+                else
+                {
+                    startcolumn = 6;
+                }
+
+                // looping through the mini grid
+                for(int i = startrow; i <= startrow + 2 && !found; i++)
+                {
+                    for(int j = startcolumn; j <= startcolumn + 2 && !found; j++)
+                    {
+                        if(board[i,j].Number == number)
+                        {
+                            found = true;
+                        }
+                    }
+                }
+                if (found)
+                {
+                    continue;
+                }
+
+                tiles.Add(new Tile(number));
+            }
+
+            return tiles;
         }
-        */
+        
         public void Print()
         {
             // TODO!
@@ -56,12 +149,26 @@ namespace Sudoku
             }
         }
 
-        public void SolveSudoku()
+        public bool SolveSudoku()
         {
-            //var emptyTile = emptyTiles[0];
-            //emptyTiles.RemoveAt(0);
+            if(emptySpots.Count == 0)
+            {
+                return true;
+            }
+
+            var emptySpot = emptySpots[0];
+            emptySpots.RemoveAt(0);
+
+            List<Tile> possible = PossibleTiles(emptySpot);
+            if(possible.Count == 0)
+            {
+                return false;
+            }
 
 
+
+
+            return false;
         }
     }
 }
--- Lab 2/Sudoku/Sudoku/Tile.cs
+++ Lab 2/Sudoku/Sudoku/Tile.cs
@@ -11,6 +11,11 @@ namespace Sudoku
             Number = Convert.ToInt32(number) - 48;
         }
 
+        public Tile(int number)
+        {
+            Number = number;
+        }
+
         public void Print()
         {
             Console.Write("{0,3}", Number);
