df5f5e282388c6bab904e10c6e44c4a59748a704
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2022-02-15 14:29:55 +0100

    started lab 3.2, created a representation for the locations and read all the locations from the file

---
+++ Lab 3/TravelingSalesMan_AntColony/Graph.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TravelingSalesMan_AntColony
+{
+    class Graph
+    {
+        public List<Location> GenerateLocations()
+        {
+            //a list that will store all the locations
+            List<Location> locations = new List<Location>();
+
+            string locationfile = "F:/Mälardalens Högskola/DVA340/Lab 3/Berlin52.tsp";
+            string[] lines = File.ReadAllLines(locationfile); //reads all the lines from the file
+            string[] substrings;
+            int i = 0;
+
+
+            // loops until we reach the position int the file we want to take information from
+            while (true)
+            {
+                if (lines[i].StartsWith("NODE_COORD_SECTION"))
+                {
+                    break;
+                }
+                i++;
+            }
+            i++;
+
+            // puts the information we want into the location list and after we return the list
+            while (lines[i] != "EOF")
+            {
+                substrings = lines[i].Split(' ');
+                locations.Add(new Location(int.Parse(substrings[0]), double.Parse(substrings[1].Replace('.', ',')), double.Parse(substrings[2].Replace('.', ','))));
+                i++;
+            }
+
+            return locations;
+        }
+    }
+}
---
+++ Lab 3/TravelingSalesMan_AntColony/Location.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelingSalesMan_AntColony
+{
+    class Location
+    {
+        public Location(int id, double x, double y)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+        }
+
+        public Location (Location other)
+        {
+            Id = other.Id;
+            X = other.X;
+            Y = other.Y;
+        }
+
+
+        public int Id { get; }
+        public double X { get; }
+        public double Y { get; }
+
+        // calculates the distance between two locations
+        public double DistanceTo(Location other) => 
+            Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+    }
+}
