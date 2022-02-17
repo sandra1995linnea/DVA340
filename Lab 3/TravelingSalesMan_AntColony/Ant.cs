4724b29819848e58da64d9d2051f0c875cba0ab6
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2022-02-17 09:55:37 +0100

    added ant and pheremone classes

---
+++ Lab 3/TravelingSalesMan_AntColony/Ant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelingSalesMan_AntColony
+{
+    class Ant
+    {
+        public Ant()
+        {
+            List<Location> locations = new List<Location>();
+            List<Location> visited = new List<Location>();
+        }
+    }
+}
