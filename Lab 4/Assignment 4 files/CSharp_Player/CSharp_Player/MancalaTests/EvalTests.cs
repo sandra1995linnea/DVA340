50dbbb21451b8ec67c6bc39522afa673f2354e3f
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2022-03-31 22:36:35 +0200

    eval done

---
+++ Lab 4/Assignment 4 files/CSharp_Player/CSharp_Player/MancalaTests/EvalTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp_Player;
+
+namespace MancalaTests
+{
+    [TestClass]
+    public class EvalTests
+    {
+        [TestMethod]
+        public void WinningBoard_MaxPlayer()
+        {
+            
+        }
+
+        [TestMethod]
+        public void WinningBoard_MinPlayer()
+        {
+
+        }
+
+        [TestMethod]
+        public void LoosingBoard_MaxPlayer()
+        {
+
+        }
+
+        [TestMethod]
+        public void LoosingBoard_MinPlayer()
+        {
+
+        }
+    }
+}
