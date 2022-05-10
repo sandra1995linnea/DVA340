ea5e75b9f3a760fb9c0264b5a0b3e620487820e7
Sandra Eriksen <59083841+sandra1995linnea@users.noreply.github.com>
2022-05-10 21:54:07 +0200

    Started on trainer class

---
+++ Lab 5/ANN/Trainer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANN
+{
+    class Trainer
+    {
+        public void Train(Net net, List<Data> trainingSet)
+        {
+
+            foreach(var example in trainingSet)
+            {
+                float[] output = net.Update(example.Pixles);
+                float[] expectedOutput = ExpectedOutput(example.Label);
+
+                //float[] errorTerms = errorTerms()
+            }
+
+            
+        }
+
+        private float[] ExpectedOutput(int label)
+        {
+            float[] expected = { -1, -1, -1, 1, -1, -1, 1, -1, -1, -1 };
+            expected[label] = 1;
+            return expected;
+        }
+    }
+}
